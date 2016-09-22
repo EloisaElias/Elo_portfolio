0bf2086d0541003d730478deed3dbc3bb7c08429
EloisaElias <eloisaelias@gmail.com>
2016-09-22 12:59:17 -0700

    DT-class

--- DecisioTree_elo.py
+++ DecisioTree_elo.py
@@ -62,7 +62,6 @@ class DecisionTree(object):
 
     return -1*sum(entropy)
 
-
   def _gini():
     '''
     Gini impurity used by CART classification and regression tree
@@ -76,11 +75,22 @@ class DecisionTree(object):
 
     return 1 - sum(gini)
 
+  def _information_gain_hs(self, y, y1, y2):
+    '''
+    gain = Entropy(parent) - Avg(entropy(children))
+    his should take the data and try every possible
+    feature and value to split on. It should find the one with the best information gain.
+    '''
+    h_ = self._h_entropy
+    gain_y = h_(y) - len(y1)/float(len(y)) * h_(y1) - len(y2)/float(len(y)) * h_(y2)
+    return gain_y
+
+
   def _make_split(self, X, y, split_index, split_value):
     '''
     INPUT:  X - 2d array
             y - 1d array
-            split_index: int(featude index)
+            split_index: int(feature index)
             split_value: int/float/bool/str (feature value)
 
     OUTPUT: X1 - 2d array
@@ -104,30 +114,46 @@ class DecisionTree(object):
     return X1, y1, X2, y2
 
 
-  def _information_gain_hs(self, y, y1, y2):
-    '''
-    gain = Entropy(parent) - Avg(entropy(children))
-    his should take the data and try every possible
-    feature and value to split on. It should find the one with the best information gain.
-    '''
-    h_ = self._h_entropy
-    gain_y = h_(y) - len(y1)/float(len(y)) * h_(y1) - len(y2)/float(len(y)) * h_(y2)
-    return gain_y
 
   def _choose_split_index(self, X, y):
+    '''
+    take the data and try every possible feature and value to split on.
+    It should find the one with the best information gain.
+    '''
     max_gain = 0
     max_value = None
     max_index = None
 
-    for i in range(X.shape[1]): # obtaining features
+    for i in range(X.shape[1]): # obtaining features (index)
 
       gain_dict = {}
-      for j in range(len(X)):
+      for j in range(len(X)): # obtain feature value X[j][i]
         if X[j][i] not in gain_dict:
           X1, y1, X2, y2 = self._make_split(X, y, i, X[j][i]) # x[0,2] = x[0][2]
           gain = self._information_gain_hs(y, y1, y2)
           gain_dict[X[j][i]] = gain
+      best_split = max(gain_dict, key= lambda K: gain_dict[K]) # best information gain
 
+      if gain_dict[best_split] > max_gain:
+        max_gain = gain_dict[best_split]
+        max_value = best_split # key with the max value
+        max_index = i # feature index
+
+    if max_gain == 0:
+      return None, None, None
+
+    return max_index, max_value, self._make_split(X, y, max_index, max_value)
+
+
+
+
+  def predict(self, X):
+    # calling predict_one
+    return np.apply_along_axis(self.root.predict_one, axis=1, arr=X)
+
+
+  def __str__(self):
+    return str(self.root)
 
 
 
